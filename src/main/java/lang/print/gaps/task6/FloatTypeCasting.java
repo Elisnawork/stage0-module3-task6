b41c855a30d498ce0d6f36c2114b4d36314ef486
naeliseev <elisnawork@gmail.com>
2025-02-03 21:54:34 +0800

    Done

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+
+        int round = (int) (numberToBeRounded + 0.5);
+        System.out.println(round);
+
     }
+
 }
